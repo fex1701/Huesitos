ccf0c554879f9cc39064e4111a39d5ba57245d7a
fex1701 <fgarcia083@soyudemedellin.edu.co>
2026-04-15 11:16:03 -0500

    Menu de inicio

--- Huesitos/Huesitos/AGENTS.md
+++ Huesitos/Huesitos/AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: Huesitos
 - Unity version: Unity 6000.3.10f1
 - Active game object:
-  - Name: Player 
-  - Tag: Player
-  - Layer: Default
+  - Name: Menu
+  - Tag: Untagged
+  - Layer: UI
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Huesitos/Huesitos/Assets/_Main/Scripts/MenuSystem.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSystem : MonoBehaviour
+{
+    
+    public void Jugar()
+    {
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+   
+    public void Salir()
+    {
+        Console.WriteLine("Salir del juego");
+        Application.Quit();
+    }
+}
