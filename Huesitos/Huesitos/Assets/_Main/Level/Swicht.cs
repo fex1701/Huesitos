7de84071d5a82f6bbc1f4a089a92c9f7fd7672f2
Juanfe456 <juanfernandezj10@gmail.com>
2026-04-28 10:57:20 -0500

    clase swicht

---
+++ Huesitos/Huesitos/Assets/_Main/Level/Swicht.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Swicht : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //crearemos un metodo que nos permita cambiar los estados del juego 
+        //pausa
+        //salir
+        //reiniciar
+        //perder
+        //play
+        //
+    }
+}
