a280dee0f9aaf0569d28aa5032765cbab8c4ce42
fex1701 <fgarcia083@soyudemedellin.edu.co>
2026-04-28 11:32:18 -0500

    Switch

--- Huesitos/Huesitos/AGENTS.md
+++ Huesitos/Huesitos/AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: Huesitos
 - Unity version: Unity 6000.3.10f1
 - Active game object:
-  - Name: Tilemap
+  - Name: Button (1)
   - Tag: Untagged
-  - Layer: Default
+  - Layer: UI
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
---
+++ Huesitos/Huesitos/Assets/_Main/Scripts/Switch.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+     
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //crearemos un metodo que me permita cambiar el estado del juego
+        // pausa
+        //play 
+        //reiniciar
+        //salir 
+        //ganar
+        //perder
+
+    }
+    public void EstadosDelJuego(int numero)
+    {
+        switch (numero)
+            {
+            case 0:
+         
+    } 
+}
