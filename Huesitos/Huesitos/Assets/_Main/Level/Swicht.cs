ad7dd825e6f4a084bcae03d2746e732de6b9900b
Juanfe456 <juanfernandezj10@gmail.com>
2026-04-29 10:47:55 -0500

    taller switch

--- Huesitos/Huesitos/Assets/_Main/Level/Swicht.cs
+++ Huesitos/Huesitos/Assets/_Main/Level/Swicht.cs
@@ -2,10 +2,11 @@ using UnityEngine;
 
 public class Swicht : MonoBehaviour
 {
+    [SerializeField] private TMPro.TMP_Text Dialogotexto;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -19,4 +20,28 @@ public class Swicht : MonoBehaviour
         //play
         //
     }
+
+    public void EstadosDelJuego(int numero)
+    {
+        switch (numero)
+        {
+            case 0:
+                Dialogotexto.text = "eres pendejo porque ño?";
+                break;
+            case 1:
+                Dialogotexto.text = "Dale espera a que regrese mi internet y jugamos";
+                break;
+            case 2: 
+                Dialogotexto.text = "No w mas tarde viene mi novia";
+                break;
+
+
+
+
+
+
+        }
+    }
 }
+
+
