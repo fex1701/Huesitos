460628571b1b60f4d6cb86834f19031ec4a2c2f5
Juanfe456 <juanfernandezj10@gmail.com>
2026-04-30 11:47:23 -0500

    preparacion script

--- Huesitos/Huesitos/AGENTS.md
+++ Huesitos/Huesitos/AGENTS.md
@@ -2,7 +2,7 @@
 - Project name: Huesitos
 - Unity version: Unity 6000.3.10f1
 - Active game object:
-  - Name: Button (2)
+  - Name: Image
   - Tag: Untagged
   - Layer: UI
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Huesitos/Huesitos/Assets/_Main/Scripts/ScriptNovela.cs
+++ Huesitos/Huesitos/Assets/_Main/Scripts/ScriptNovela.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+
+    [SerializeField] private TMPro.TMP_Text Dialogotexto;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -12,5 +14,27 @@ public class NewMonoBehaviourScript : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void EstadosDelJuego(int numero)
+    {
+        switch (numero)
+        {
+            case 0:
+                Dialogotexto.text = "eres pendejo porque ño?";
+                break;
+            case 1:
+                Dialogotexto.text = "Dale espera a que regrese mi internet y jugamos";
+                break;
+            case 2:
+                Dialogotexto.text = "No w mas tarde viene mi novia";
+                break;
+
+
+
+
+
+
+        }
     }
 }
