233ec3b4beaa748d0d67293d2abc226ddc7c149e
fex1701 <fgarcia083@soyudemedellin.edu.co>
2026-05-07 11:57:11 -0500

    Ejercicio de orden de sprites

--- Huesitos/Huesitos/Assets/_Main/Scripts/Novela grafica/SpriteCharacterController.cs
+++ Huesitos/Huesitos/Assets/_Main/Scripts/Novela grafica/SpriteCharacterController.cs
@@ -1,13 +1,50 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class SpriteCharacterController : MonoBehaviour
 {
-   [SerializeField] private SpriteRenderer _mainRender;
-   [SerializeField] private SpriteRenderer _ruborRender;
-   [SerializeField] private SpriteRenderer _enojoRender;
-   [SerializeField] private SpriteRenderer _confundidoRender;
+
+    [Header("Array SpriteRender")]
+
+    [SerializeField]
+    private SpriteRenderer[] spriteRenderers;
+
     [Header("Sprite")]
-   [SerializeField] private Sprite _main;
+
+    [SerializeField]
+    private Sprite _mainSprite;
+
+
+    [SerializeField]
+    private Sprite _hairSprite;
+
+    [SerializeField]
+    private Sprite _whiteSprite;
+
+
+
+
+
+    [Header("Array Sprite")]
+    [SerializeField]
+    private Sprite[] _tearsSprites;
+
+    [SerializeField]
+    private Sprite[] _mouthSprites;
+
+    [SerializeField]
+    private Sprite[] _graphicSprites;
+
+    [SerializeField]
+    private Sprite[] _eyedilsSprites;
+
+    [SerializeField]
+    private Sprite[] _pupilSprites;
+
+    [SerializeField]
+    private Sprite[] _eyebrowsSprites;
+
+    [SerializeField]
+    private Sprite[] _cheeksSprites;
     void Start()
     {
         
